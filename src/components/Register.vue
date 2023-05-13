1e26023ba093bb6ec63bcd8b6fbf507b9f312dcd
Youness213 <50671181+Youness213@users.noreply.github.com>
2023-05-13 03:28:34 +0100

    last

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -3,59 +3,25 @@
     <v-snackbar v-model="snackbar" top color="warning" flat>
       <span>Cette adresse email est déjà utiliser</span>
     </v-snackbar>
-      <v-img class="align-center ml-16 mr-n16 px-15"
-      cover
-      width="1710"
-        src="@/assets/3725.jpg"
-      >
-      <v-card
-          class="mx-auto"
-          max-width="400"
-          title="Création d'un compte"
-        >
+    <v-img class="align-center ml-16 mr-n16 px-15" cover width="1710" src="@/assets/3725.jpg">
+      <v-card class="mx-auto" max-width="400" title="Création d'un compte">
         <v-form @submit.prevent ref="form">
           <v-container>
-            <v-text-field
-              v-model="user.first"
-              :rules="[required]"
-              color="primary"
-              label="Nom"
-              variant="underlined"
-            ></v-text-field>
+            <v-text-field v-model="user.first" :rules="[required]" color="primary" label="Nom"
+              variant="underlined"></v-text-field>
 
-            <v-text-field
-              v-model="user.last"
-              :rules="[required]"
-              color="primary"
-              label="Prénom"
-              variant="underlined"
-            ></v-text-field>
+            <v-text-field v-model="user.last" :rules="[required]" color="primary" label="Prénom"
+              variant="underlined"></v-text-field>
 
-            <v-text-field
-              v-model="user.email"
-              :rules="[required]"
-              color="primary"
-              label="Email"
-              variant="underlined"
-            ></v-text-field>
+            <v-text-field v-model="user.email" :rules="[required]" color="primary" label="Email"
+              variant="underlined"></v-text-field>
 
-            <v-text-field
-              v-model="user.password"
-              :rules="[required]"
-              :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
-              :type="show ? 'text' : 'password'"
-              @click:append="show = !show"
-              color="primary"
-              label="Mot de passe"
-              variant="underlined"
-            ></v-text-field>
+            <v-text-field v-model="user.password" :rules="[required]" :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
+              :type="show ? 'text' : 'password'" @click:append="show = !show" color="primary" label="Mot de passe"
+              variant="underlined"></v-text-field>
 
-            <v-checkbox
-              v-model="terms"
-              :rules="[required]"
-              color="success"
-              label="I agree to site terms and conditions"
-            ></v-checkbox>
+            <v-checkbox v-model="terms" :rules="[required]" color="success"
+              label="I agree to site terms and conditions"></v-checkbox>
           </v-container>
 
           <v-divider></v-divider>
@@ -66,7 +32,7 @@
               Se Connecter
 
             </v-btn>
-            
+
             <v-spacer></v-spacer>
             <v-divider vertical></v-divider>
             <v-divider vertical></v-divider>
@@ -80,80 +46,80 @@
             </v-btn>
           </v-card-actions>
         </v-form>
-        </v-card>
-      </v-img>
+      </v-card>
+    </v-img>
   </div>
 </template>
   
 <script>
 import axios from 'axios'
-
-  export default {
-    data: () => ({
-      form: true,
-      user  :{
-      first:'',
-      last:'',
+export default {
+  data: () => ({
+    form: true,
+    user: {
+      first: '',
+      last: '',
       email: '',
       password: '',
-      status:false
-      },
-      loading: false,
-      snackbar:false,
-      show: false,
-      terms:false
-    }),
+      status: false
+    },
+    loading: false,
+    snackbar: false,
+    show: false,
+    terms: false
+  }),
 
-    methods: {
-      async onSubmit () {
-        try {
-          var valid =(await this.$refs.form.validate()).valid
-          await axios.get('https://backendfortasksquad13.onrender.com/api/getuser').then(r => {
-            r.data.forEach(element => {
-              if(element.email == this.email){
-                valid = false
-                this.snackbar = true
-              }
-            });
-          })
-          if (valid) {
-            axios.post('https://backendfortasksquad13.onrender.com/api/create-users',this.user)
+  methods: {
+    async onSubmit() {
+      try {
+        var valid = (await this.$refs.form.validate()).valid
+        await axios.get('https://backendfortasksquad13.onrender.com/api/getuser').then(r => {
+          r.data.forEach(element => {
+            if (element.email == this.email) {
+              valid = false
+              this.snackbar = true
+            }
+          });
+        })
+        if (valid) {
+          axios.post('https://backendfortasksquad13.onrender.com/api/create-users', this.user)
             .then(async (res) => {
-              if(res.status ==200){
-                
+              if (res.status == 200) {
+
                 await this.Sendit()
                 this.$router.push('/login')
                 this.user = {
-                  first:'',
-                  last:'',
+                  first: '',
+                  last: '',
                   email: '',
                   password: '',
-                  status:false
+                  status: false
                 }
               }
             })
 
-          }
         }
-        catch{
-          console.log('nope')
-        }
-      },
-      required (v) {
-        return !!v || 'Champ requis'
-      },
-      async Sendit() {
-        /*await Email.send({
-          Host: "smtp.elasticemail.com",
-          Port: '2525',
-          Username: "tasksquad13000@gmail.com",
-          Password: "8AC08D1583A27406869865CBDB55A9209811",
-          To: this.user.email,
-          From: "tasksquad13000@gmail.com",
-          Subject: "Email d'activation",
-          Body: "Votre compte viens d'être activer",
-        });*/
-      },
+      }
+      catch {
+        console.log('nope')
+      }
     },
-  }
+    required(v) {
+      return !!v || 'Champ requis'
+    },
+    async Sendit() {
+      await axios.post('https://backendfortasksquad13.onrender.com/email/send',{email:this.user.email,subject:"Email d'activation",text: "Votre compte viens d'être activer"}).then(r=>{console.log(r)})
+      /*await Email.send({
+        Host: "smtp.elasticemail.com",
+        Port: '2525',
+        Username: "tasksquad13000@gmail.com",
+        Password: "8AC08D1583A27406869865CBDB55A9209811",
+        To: this.user.email,
+        From: "tasksquad13000@gmail.com",
+        Subject: "Email d'activation",
+        text: "Votre compte viens d'être activer",
+      });*/
+    },
+  },
+}
 </script>
